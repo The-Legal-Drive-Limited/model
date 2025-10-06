502bcc620fdc52a35629a79a449ecca1c576d061
Eliot Stock <1144814+eliotstock@users.noreply.github.com>
2025-10-06 16:58:14 +1300

    TODO

--- generate.sh
+++ generate.sh
@@ -4,4 +4,5 @@
 datamodel-codegen --input-file-type jsonschema --input schemas/index.json --output gen/python/lawdbl_model/model.py
 
 # Typescript - run from schemas directory
+# TODO: Look into switching to json-schema-to-typescript and forcing the use of enums rather than union types, to make iterating over the values easier in client code.
 cd schemas && npx json2ts --input index.json --output ../gen/ts/model.ts && cd ..
