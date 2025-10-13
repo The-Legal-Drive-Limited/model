ef549f13452320be4185580fef31f8d0b95449af
Eliot Stock <1144814+eliotstock@users.noreply.github.com>
2025-10-13 18:55:11 +1300

    FieldOfLaw now one or more

--- gen/ts/model.ts
+++ gen/ts/model.ts
@@ -25,7 +25,7 @@ export interface Lawyer {
   [k: string]: unknown;
 }
 export interface Query {
-  fieldOfLaw?: FieldOfLaw;
+  fieldOfLaw?: FieldOfLaw | [FieldOfLaw, ...FieldOfLaw[]];
   /**
    * The minimum required years of practice.
    */
